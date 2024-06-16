d377c4416d3d9ea891a032847cddad75c7866ac5
freek-heining <60843505+freek-heining@users.noreply.github.com>
2024-06-16 19:36:03 +0200

    defaults

--- Script.lua
+++ Script.lua
@@ -154,12 +154,12 @@ function onload(state)
         TurnOrderTable = decodedState.variables.turnOrderTable
     end
 
-    UI.setAttribute("setupWindow", "active", false) -- ENABLE when developing
+    --UI.setAttribute("setupWindow", "active", false) -- ENABLE when developing
     
-    --SetInteractableFalse() -- Initially set lots of components to interactable = false 
+    SetInteractableFalse() -- Initially set lots of components to interactable = false 
 
     if not setupDone then
-        --MoveHandZones("+", 300) -- DISABLE when developing board! Move away temporary so nobody selects color manually. 
+        MoveHandZones("+", 300) -- DISABLE when developing board! Move away temporary so nobody selects color manually. 
     else
         UI.setAttribute("setupWindow", "active", false)
     end
